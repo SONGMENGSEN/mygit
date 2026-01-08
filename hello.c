d3bebfe8663ed97275104182ec1856410e3a97bc
sms <1013939766@qq.com>
2026-01-08 15:53:51 +0800

    更改了错误

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 
 int main() {
     printf("Hello, World ,紧急修复了个bug!\n");
-    printf("Hello, Wolrd!\n");
+    printf("Hello, World!\n");
     printf("Hello, World 2!\n");
     printf("Hello, World 3!\n");
     return 0;
